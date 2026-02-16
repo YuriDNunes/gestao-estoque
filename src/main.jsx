aa3db4df362bfeeb5d62a1098ca1f57eb896c752
Yuri <ndcyuri@gmail.com>
2026-02-16 10:02:37 -0300

    refactor: routes configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+import "./styles/Global.css";
+
+import AuthLayout from "./layouts/AuthLayout.jsx";
+import Login from "./pages/Login.jsx";
+import AdminLayout from "./layouts/AdminLayout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
 
