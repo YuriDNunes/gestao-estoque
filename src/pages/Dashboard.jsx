43381df5cf613202ba19e014386770baa820270f
Yuri <ndcyuri@gmail.com>
2026-02-18 18:25:45 -0300

    feat: created dashboard mock

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,15 @@
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  return <div className={styles.Dashboard}>Dashboard</div>;
+  return (
+    <div className={styles.Dashboard}>
+      <div className={styles.container}>
+        <div className={styles.card}>1</div>
+        <div className={styles.card}>2</div>
+        <div className={styles.card}>3</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
