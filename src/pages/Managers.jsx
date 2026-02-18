944cc63315fdc471364c12a6b52bde6f84218d68
Yuri <ndcyuri@gmail.com>
2026-02-18 18:02:40 -0300

    feat: added form to managers page

--- src/pages/Managers.jsx
+++ src/pages/Managers.jsx
@@ -1,7 +1,15 @@
+import Form from "../components/Form";
 import styles from "./Manager.module.css";
 
 const Managers = () => {
-  return <div className={styles.Manager}>Managers</div>;
+  return (
+    <div className={styles.Manager}>
+      <div className={styles.formCard}>
+        <Form />
+      </div>
+      <div>Managers list</div>
+    </div>
+  );
 };
 
 export default Managers;
