078d73ce2bf44aff05978b28ee9cf325f3b2f33e
Yuri <ndcyuri@gmail.com>
2026-03-11 16:28:03 -0300

    feat(main): added MUI ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import "./styles/Global.css";
 
@@ -11,6 +13,7 @@ import Dashboard from "./pages/Dashboard.jsx";
 import Managers from "./pages/Managers.jsx";
 import Users from "./pages/Users.jsx";
 import Products from "./pages/Products.jsx";
+import theme from "./theme/index.js";
 
 const router = createBrowserRouter([
   {
@@ -48,7 +51,8 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <RouterProvider router={router} />
-  </StrictMode>,
+  </ThemeProvider>,
 );
