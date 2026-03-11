01c9ee554c5490e11ce728551693bdd189c5c85f
Yuri <ndcyuri@gmail.com>
2026-03-11 16:18:14 -0300

    feat(theme): created initial theme index.js

---
+++ src/theme/index.js
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1e3a5f",
+      contrastText: "#fff",
+    },
+    background: {
+      default: "#f0f4f8",
+      paper: "#fff",
+    },
+  },
+  typography: {},
+  shape: {},
+  components: {},
+});
+
+export default theme;
