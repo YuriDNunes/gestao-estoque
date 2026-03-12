a3fff2bd92fde839e8fceedfa56c3dbfa379c67c
Yuri <ndcyuri@gmail.com>
2026-03-12 17:16:26 -0300

    refactor(Users): migrate Users to MUI

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,71 @@
-import styles from "./Users.module.css";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import Divider from "@mui/material/Divider";
 
 const Users = () => {
-  return <div className={styles.Users}>Users</div>;
+  return (
+    <Box sx={{ p: 3 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h3" color="text.primary">
+          Usuários
+        </Typography>
+        <Button variant="contained" startIcon={<AddRoundedIcon />}>
+          Novo usuário
+        </Button>
+      </Box>
+
+      <Divider sx={{ my: 2.5 }} />
+
+      <TableContainer component={Paper} sx={{ pt: 1, pb: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Registro</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Cargo</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>U001</TableCell>
+              <TableCell>Maria</TableCell>
+              <TableCell>maria@empresa.com</TableCell>
+              <TableCell>Usuário</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>U002</TableCell>
+              <TableCell>Felipe</TableCell>
+              <TableCell>felipe@empresa.com</TableCell>
+              <TableCell>Usuário</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>U003</TableCell>
+              <TableCell>Jeffin</TableCell>
+              <TableCell>jeffin@empresa.com</TableCell>
+              <TableCell>Usuário</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 };
 
 export default Users;
