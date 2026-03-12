12de5ec4eec03886ba6d20ffb3dc1568d9f7e6fd
Yuri <ndcyuri@gmail.com>
2026-03-12 16:30:48 -0300

    refactor(AdminLayout): migrate AdmiunLayoyt to MUI

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,12 +1,23 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
+import { useState } from "react";
+import Box from "@mui/material/Box";
 
 const AdminLayout = () => {
+  const [open, setOpen] = useState(true);
+
   return (
-    <div>
-      <SideBar />
-      <Outlet />
-    </div>
+    <Box>
+      <SideBar open={open} onToggle={() => setOpen(!open)} />
+      <Box
+        sx={{
+          ml: open ? "3rem" : "-6rem",
+          transition: "margin 0.2s ease",
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
   );
 };
 
