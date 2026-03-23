79d7fe7efc0f6f12ad4d75a7082f89d5124892d5
Yuri <ndcyuri@gmail.com>
2026-03-23 14:59:03 -0300

    feat(Products): created table in products in MUI

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,67 @@
-import styles from "./Product.module.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Typography from "@mui/material/Typography";
+
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
 
 const Products = () => {
-  return <div className={styles.Product}>Products</div>;
+  return (
+    <Box sx={{ p: 3 }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h3" color="text.primary">
+          Produtos
+        </Typography>
+        <Button variant="contained" startIcon={<AddRoundedIcon />}>
+          Novo produto
+        </Button>
+      </Box>
+
+      <Divider sx={{ my: 2.5 }} />
+
+      <TableContainer component={Paper} sx={{ pt: 1, pb: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Código</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>Quantidade</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>P001</TableCell>
+              <TableCell>Mouses</TableCell>
+              <TableCell>10</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>P002</TableCell>
+              <TableCell>Teclados</TableCell>
+              <TableCell>15</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>P003</TableCell>
+              <TableCell>Monitores</TableCell>
+              <TableCell>5</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 };
 
 export default Products;
