191254d9df353063dea36570931f679367c9407a
Yuri <ndcyuri@gmail.com>
2026-03-25 17:08:28 -0300

    feat(Dashboard): add metrics cards layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,61 @@
-import styles from "./Dashboard.module.css";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const Dashboard = () => {
   return (
-    <div className={styles.Dashboard}>
-      <div className={styles.container}>
-        <div className={styles.card}>1</div>
-        <div className={styles.card}>2</div>
-        <div className={styles.card}>3</div>
-      </div>
-    </div>
+    <Grid container spacing={2} margin={3}>
+      <Grid item size={4}>
+        <Card>
+          <CardContent sx={{ padding: 4 }}>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography variant="body2" color="text.secondary">
+                Gestor
+              </Typography>
+              <Typography variant="h3">4</Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item size={4}>
+        <Card>
+          <CardContent sx={{ padding: 4 }}>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography variant="body2" color="text.secondary">
+                Usuarios
+              </Typography>
+              <Typography variant="h3">4</Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item size={4}>
+        <Card>
+          <CardContent sx={{ padding: 4 }}>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography variant="body2" color="text.secondary">
+                Produtos
+              </Typography>
+              <Typography variant="h3">4</Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item size={12}>
+        <Card>
+          <CardContent>
+            <Box sx={{ textAlign: "center" }}>
+              <Typography variant="body2" color="text.secondary">
+                Registros do mês
+              </Typography>
+              <Typography variant="h3">12</Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
