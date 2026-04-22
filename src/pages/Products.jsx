39511c40e4e83576c37fa1837cd10a35ba0bfd98
Yuri <ndcyuri@gmail.com>
2026-04-22 13:53:49 -0300

    feat: created dialog form

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,8 +11,24 @@ import Typography from "@mui/material/Typography";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import Paper from "@mui/material/Paper";
 import Divider from "@mui/material/Divider";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 const Products = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box sx={{ p: 3 }}>
       <Box
@@ -25,10 +41,58 @@ const Products = () => {
         <Typography variant="h3" color="text.primary">
           Produtos
         </Typography>
-        <Button variant="contained" startIcon={<AddRoundedIcon />}>
+        <Button
+          variant="contained"
+          startIcon={<AddRoundedIcon />}
+          onClick={handleOpen}
+        >
           Novo produto
         </Button>
       </Box>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Cadastre um novo produto</DialogTitle>
+        <DialogContent>
+          <form>
+            <TextField
+              autoFocus
+              required
+              id="code"
+              name="code"
+              label="Código do produto"
+              type="text"
+              variant="standard"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              required
+              id="name"
+              name="name"
+              label="Informe o nome do produto"
+              type="text"
+              variant="standard"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              required
+              id="quantity"
+              name="quantity"
+              label="Informe a quantidade"
+              type="number"
+              variant="standard"
+              fullWidth
+            />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="contained">Cadastrar</Button>
+        </DialogActions>
+      </Dialog>
 
       <Divider sx={{ my: 2.5 }} />
 
